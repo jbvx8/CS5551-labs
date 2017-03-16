31dc34b804aa1654d936bf829db62a98c3ad90a1
Jackie <jbvx8@mail.umkc.edu>
2017-03-15 23:15:40 -0500

    lab8 changes

--- Source/Lab7/www/js/app.js
+++ Source/Lab7/www/js/app.js
@@ -3,10 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var Lab7 = angular.module('starter', ['ionic', 'iongCordova', 'firebase']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+Lab7.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -21,4 +21,80 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+Lab7.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state("firebase", {
+            url: "/firebase",
+            templateUrl: "templates/firebase.html",
+            controller: "FirebaseController",
+            cache: false
+        })
+        .state("secure", {
+            url: "/secure",
+            templateUrl: "templates/secure.html",
+            controller: "SecureController"
+        });
+    $urlRouterProvider.otherwise('/firebase');
+});
+
+Lab7.controller("FirebaseController", function ($scope, $state, $firebaseAuth) {
+    var fbAuth = $firebaseAuth();
+    
+    $scope.login = function(username, password) {      fbAuth.$signInWithEmailAndPassword(username,password).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+    $scope.register = function(username, password) {
+        fbAuth.$createUserWithEmailAndPassword(username, password).then(function(userData) {
+            return fbAuth.$signInWithEmailAndPassword(username, password);
+        }).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }     
+});
+                
+Lab7.controller("SecureController", function($scope, $ionicHistory, $firebaseObject, $firebaseArray, $firebaseAuth, $cordovaCamera,$state) {
+
+    $ionicHistory.clearHistory();  //for clearing user login history
+
+    $scope.images = [];
+    $scope.fb = $firebaseAuth();
+    var fbAuth = $scope.fb.$getAuth();
+    var ref = firebase.database().ref();
+    var obj = $firebaseObject(ref);
+    if(fbAuth) {
+        var userReference = ref.child("users/" + fbAuth.uid);   //capture the user reference in data structure ,it navigates to specific user page in freebase
+        var syncArray = $firebaseArray(userReference.child("images"));  //binding specific node in firebase to an array object in angularjs
+        $scope.images = syncArray;
+    } else {
+        $state.go("firebase");  //directs to firebase page
+    }
+
+    $scope.upload = function() {
+        var options = {
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+            targetWidth: 500,
+            targetHeight: 500,
+            saveToPhotoAlbum: false
+        };
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            syncArray.$add({image: imageData}).then(function() {
+                alert("Image has been uploaded");
+            });
+        }, function(error) {
+            console.error(error);
+        });
+    }
+
+});
