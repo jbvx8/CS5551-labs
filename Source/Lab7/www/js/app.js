1805cb750def8754268a45596926c5c0c57e83a0
Jackie <jbvx8@mail.umkc.edu>
2017-03-15 23:42:52 -0500

    fix bug

--- Source/Lab7/www/js/app.js
+++ Source/Lab7/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var Lab7 = angular.module('starter', ['ionic', 'iongCordova', 'firebase']);
+var Lab7 = angular.module('starter', ['ionic', 'ngCordova', 'firebase']);
 
 Lab7.run(function ($ionicPlatform) {
   $ionicPlatform.ready(function () {
